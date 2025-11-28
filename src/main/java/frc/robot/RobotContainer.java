40039aabf80dc3204f974f83af1d096190231a3d
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2025-11-28 01:30:52 -0800

    implement outtake commands in robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,16 +6,27 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.OuttakeSubsystem;
 
 public class RobotContainer {
-  private final OuttakeSubsystem outtakeSubsystem = new OuttakeSubsystem();
+  private final OuttakeSubsystem m_outtakeSubsystem = new OuttakeSubsystem();
+
+  private final CommandPS5Controller m_driverController = new CommandPS5Controller(
+      OperatorConstants.kDriverControllerPort);
 
   public RobotContainer() {
     configureBindings();
+
+    m_outtakeSubsystem.setDefaultCommand(
+        m_outtakeSubsystem.VelocityControl(() -> m_driverController.getL2Axis(), () -> m_driverController.getR2Axis()));
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_driverController.povUp().onTrue(m_outtakeSubsystem.stepUp());
+    m_driverController.povDown().onTrue(m_outtakeSubsystem.stepDown());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
