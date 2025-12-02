c1a9242b4a921683c089708564154b3ad00011b0
vinthebest <vinthebest.ptlrl@gmail.com>
2025-12-01 18:24:05 -0800

    Reworked Manual and autoSetIntake. auto is now based on position. Need to do PID

--- SeventhShopProject/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ SeventhShopProject/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -8,10 +8,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.hardware.CANdi;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix6.controls.*;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+
 
 //change auto to use setposition instead of velocity
 //manual not necessary
@@ -21,50 +23,81 @@ public class IntakeSubsystem extends SubsystemBase {
   
   private TalonFX leverMotor = new TalonFX( /*insert numer */ 3);
   private CANdi limit = new CANdi(/*insert number */ 2);
-  private boolean setYet = false;
-  private double upperLim = /*decide val */ 50; //check movearm to change value, 50 is just exorbitantly large random number, but check signage here
+  private boolean autoOn = false;
+  private double upperLim = /*decide val */ 3.5; //check movearm to change value, 50 is just exorbitantly large random number, but check signage here
   private double invert = 1; //to reverse direction, just change 1 to -1
-  private boolean goingUp = false;
   DoublePublisher pos;
+  
+  
 
   
   public IntakeSubsystem() {
     leverMotor.setPosition(0.0);
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    NetworkTable table = inst.getTable("data");
+    pos = table.getDoubleTopic("pos").publish();
+  }
+
+  public void manual(boolean left, boolean right){
+    if(left && right){
+      leverMotor.set(0);
+      leverMotor.setControl(new PositionVoltage(leverMotor.getPosition().getValueAsDouble()));
+      autoOn = true;
+      return;
+    }
+
+    if(left && !right ){
+      leverMotor.set(-0.1*invert);
+    }else if(!left && right){
+      leverMotor.set(0.1*invert);
+    }else{
+      leverMotor.set(0.0*invert);
+      leverMotor.setControl(new PositionVoltage(leverMotor.getPosition().getValueAsDouble()));
+    }
   }
 
   public void autoSetIntake(boolean left, boolean right){
-    if(left && !right ){  
+
+    if(left && right){
+      leverMotor.set(0);
+      leverMotor.setControl(new PositionVoltage(leverMotor.getPosition().getValueAsDouble()));
+      autoOn = false;
+      return;
+    }
+
+    if(left && !right ){
+      if(leverMotor.getPosition().getValueAsDouble() > -0.5){// check sign on upperLim
         leverMotor.set(-0.1*invert);
-      
+      }else{
+        leverMotor.setControl(new PositionVoltage(0.0));
+      }
     }else if(!left && right){
-        if(leverMotor.getPosition().getValueAsDouble() < upperLim){// check sign on upperLim
-          leverMotor.set(0.1*invert);
-        }
-      
-    }else{
-      leverMotor.set(0.0);
+        leverMotor.setControl(new PositionVoltage(0.0));  
     }
 
   }
 
   public void moveArm(boolean left, boolean right){
-    if(!setYet){
+
+    pos.set(leverMotor.getPosition().getValueAsDouble());
+
+    if(!autoOn){
       manual(left, right);
     }
 
     if(limit.getS1Closed().refresh().getValue()){ //check is bool is true or false when pressed
-      setYet = true;
-      leverMotor.setPosition(0.0); 
       leverMotor.set(0.0);
-      upperLim = 2;                                       //check signage and number
+      leverMotor.setPosition(0.0); 
+      leverMotor.setControl(new PositionVoltage(0.0));
     }
 
     if(leverMotor.getPosition().getValueAsDouble() > upperLim){//check signage
       leverMotor.set(0.0);
+      leverMotor.setControl(new PositionVoltage(3.5));
     }
 
-    if(setYet){
-      auto(left, right);
+    if(autoOn){
+      autoSetIntake(left, right);
     }
 
   }
